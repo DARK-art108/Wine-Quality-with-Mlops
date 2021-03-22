582cb2c81727e040801735ee9e88891dbd35d7ac
DARK-art108 <daydreamingguy941@gmail.com>
2021-03-22 22:55:03 +0530

    Enhaced Test

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version = "0.0.1",
+    description="Its a Wine-Ops Package",
+    author = "CycloneLights",
+    packages = find_packages(), ##We can also specify list for hardcoding
+    license="MIT"
+
+)
